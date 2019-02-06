7ef852221b6b54e6efef4633b6fb92cb64c1da34
Victor Sebastian Mølschau Lauridsen <vict4694@edu.eal.dk>
2019-02-06 11:46:52 +0100

    VIRK NU FOR HELVEDE

---
+++ DateUnitTestProject/UnitTestDate.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using DateClassLibrary;
+
+//namespace DateUnitTestProject
+//{
+//    [TestClass]
+//    public class UnitTestDate
+//    {
+//        [TestMethod]
+//        public void TestMethod_DateConstructor()
+//        {
+//            UclDate aDate = new UclDate(2019, 2, 6);
+//            Assert.IsNotNull(aDate, "UclDate Constructor is not working");
+//        }
+
+//    }
+//}
